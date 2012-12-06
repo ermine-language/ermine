25419a072c4534fe5efe482b703d2e9396f8e687
Edward Kmett <ekmett@gmail.com>
2012-12-06 12:34:47 -0500

    type haddock pass

--- Ermine/Type.hs
+++ Ermine/Type.hs
@@ -48,6 +48,7 @@ import Text.Trifecta.Diagnostic.Rendering.Prim
 
 type FieldName = String
 
+-- | A 'Type' that can be compared with mere structural equality.
 data HardType
   = Tuple !Int -- (,...,)   :: forall (k :: @). k -> ... -> k -> k -- n >= 2
   | Arrow      -- (->) :: * -> * -> *
@@ -55,6 +56,7 @@ data HardType
   | ConcreteRho (Set FieldName)
   deriving (Eq, Ord, Show)
 
+-- | Smart constructors that allows us to pun various 'HardType' constructor names for 'Type'.
 class Typical t where
   hardType :: HardType -> t
 
@@ -118,6 +120,7 @@ instance Show k => Show1 (Type k) where showsPrec1 = showsPrec
 instance Eq2 Type where (==##) = (==)
 instance Show2 Type where showsPrec2 = showsPrec
 
+-- | Perform simultaneous substitution on kinds and types in a 'Type'.
 bindType :: (k -> Kind k') -> (a -> Type k' b) -> Type k a -> Type k' b
 bindType _ g (Var a)          = g a
 bindType f g (App l r)        = App (bindType f g l) (bindType f g r)
@@ -134,6 +137,7 @@ instance Monad (Type k) where
   return = Var
   m >>= g = bindType Kind.Var g m
 
+-- | Provide a 'Traversal' of type variables that can be used to extract them or substitute them for other type variables.
 class HasTypeVars s t a b | s -> a, t -> b, s b -> t, t a -> s where
   typeVars :: Traversal s t a b
 
@@ -152,12 +156,15 @@ instance HasTypeVars s t a b => HasTypeVars (Map k s) (Map k t) a b where
 instance HasTypeVars (TK k a) (TK k b) a b where
   typeVars = traverse
 
+-- | 'TK' is a special version of 'Scope' for types that binds kinds. It is used by 'Forall'.
 newtype TK k a = TK { runTK :: Type (Var Int (Kind k)) a }
   deriving (Eq, Show, Functor, Foldable, Traversable)
 
+-- | Embed a type which does not reference the freshly bound kinds into 'TK'.
 liftTK :: Type k a -> TK k a
 liftTK = TK . first (F . return)
 
+-- | Substitute kind variables in a 'TK', leaving the bound kinds untouched.
 bindTK :: (k -> Kind k') -> TK k a -> TK k' a
 bindTK f = TK . bindType (return . fmap (>>= f)) Var . runTK
 
@@ -180,12 +187,20 @@ instance HasKindVars (TK k a) (TK k' a) k k' where
 instance Eq k => Eq1 (TK k) where (==#) = (==)
 instance Show k => Show1 (TK k) where showsPrec1 = showsPrec
 
+-- instance BoundBy (TK k) (Type k) where
+--   bindBy f (TK b) =
+
+-- instance MangledBy (TK k) (Type k) where
+--   mangledBy f (TK b) =
+
+-- | Bind some of the kinds referenced by a 'Type'.
 abstractKinds :: (k -> Maybe Int) -> Type k a -> TK k a
 abstractKinds f t = TK (first k t) where
   k y = case f y of
     Just z -> B z
     Nothing -> F (return y)
 
+-- | Instantiate the kinds bound by a 'TK' obtaining a 'Type'.
 instantiateKinds :: (Int -> Kind k) -> TK k a -> Type k a
 instantiateKinds k (TK e) = bindType go Var e where
   go (B b) = k b
