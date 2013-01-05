252c6498be658954bda5f8f8aef35bb7ce528af7
Edward Kmett <ekmett@gmail.com>
2013-01-04 23:50:51 -0500

    more repl work

--- Main.hs
+++ Main.hs
@@ -1,16 +1,27 @@
 module Main where
 
+import Paths_ermine
+import Control.Monad.Trans
 import System.Console.Haskeline
+import System.FilePath
 
 main :: IO ()
-main = runInputT defaultSettings loop where
-  loop :: InputT IO ()
-  loop = do
-    minput <- getInputLine "% "
-    case minput of
-      Nothing     -> return ()
-      Just "quit" -> return ()
-      Just ""     -> loop
-      Just input -> do
-        outputStrLn $ "Input was: " ++ input
-        loop
+main = runInputT defaultSettings { historyFile = Just ".ermine.history" } $ do
+  logo
+  loop
+
+logo :: InputT IO ()
+logo = do
+  txt <- liftIO $ do Paths_ermine.getDataFileName ("data" </> "logo.txt") >>= readFile
+  outputStrLn txt
+
+loop :: InputT IO ()
+loop = do
+  minput <- getInputLine ">> "
+  case minput of
+    Nothing     -> return ()
+    Just "quit" -> return ()
+    Just ""     -> loop
+    Just input -> do
+      outputStrLn $ "Input was: " ++ input
+      loop
