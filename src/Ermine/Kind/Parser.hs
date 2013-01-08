afad4563fd502478619eb5e91a2d713e1d7f7fb9
Edward Kmett <ekmett@gmail.com>
2013-01-08 12:56:40 -0500

    Added haddocks

--- src/Ermine/Kind/Parser.hs
+++ src/Ermine/Kind/Parser.hs
@@ -1,3 +1,14 @@
+--------------------------------------------------------------------
+-- |
+-- Module    :  Ermine.Kind.Parser
+-- Copyright :  (c) Edward Kmett and Dan Doel 2012
+-- License   :  BSD3
+-- Maintainer:  Edward Kmett <ekmett@gmail.com>
+-- Stability :  experimental
+-- Portability: non-portable
+--
+-- This module provides the parser for kinds.
+--------------------------------------------------------------------
 module Ermine.Kind.Parser
   ( kind
   ) where
@@ -10,11 +21,13 @@ import Text.Parser.Combinators
 import Text.Parser.Token
 import Text.Parser.Token.Style
 
+-- | The internal token style used for kind variables
 kid :: TokenParsing m => IdentifierStyle m
 kid = haskellIdents
   & styleName  .~ "kind variable"
   & styleReserved .~ fromList ["rho","ρ","phi","φ","constraint","Γ"]
 
+-- | Parse an atomic kind (everything but arrows)
 kind0 :: (Monad m, TokenParsing m) => m (Kind String)
 kind0 = parens kind
     <|> star <$ symbol "*"
@@ -26,5 +39,6 @@ kind0 = parens kind
     <|> constraint <$ reserve kid "Γ"
     <|> Var <$> ident kid
 
+-- | Parse a 'Kind'.
 kind :: (Monad m, TokenParsing m) => m (Kind String)
 kind = chainr1 kind0 ((:->) <$ symbol "->")
