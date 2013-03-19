93c62f5e0b58b64bba9a10b45d276560307f91cd
Dan Doel <dan.doel@gmail.com>
2013-03-19 14:50:16 -0400

    More declaration parsing machinery

--- src/Ermine/Parser/Term.hs
+++ src/Ermine/Parser/Term.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE PatternGuards #-}
 --------------------------------------------------------------------
 -- |
 -- Module    :  Ermine.Parser.Term
@@ -16,11 +17,15 @@ module Ermine.Parser.Term ( anyType
                           , terms
                           ) where
 
-import Control.Lens ((??))
+import Control.Lens hiding (op)
 import Control.Applicative
+import Control.Comonad
+import Control.Monad.State
 import Data.Function
 import Data.Either (partitionEithers)
-import Data.List (groupBy)
+import Data.List (groupBy, find)
+import Data.Set as Set hiding (map)
+import Data.Foldable (foldrM)
 import Data.Traversable hiding (mapM)
 import Ermine.Builtin.Pat
 import Ermine.Builtin.Term
@@ -85,21 +90,60 @@ terms = commaSep term
 typeDecl :: (Monad m, TokenParsing m) => m (String, Ann)
 typeDecl = (,) <$> try (ident termIdent) <*> annotation
 
-termDeclClause :: (Monad m, TokenParsing m) => m (String, [PP], Tm)
-termDeclClause = (,,) <$> ident termIdent <*> many pat0 <* reserve op "=" <*> term
+termDeclClause :: (Monad m, TokenParsing m) => m (String, Binder String [Pat Ann], Tm)
+termDeclClause = (,,) <$> ident termIdent <*> pat0s <* reserve op "=" <*> term
+ where
+ pat0s = do ps <- sequenceA <$> many pat0
+            ps <$ validate ps
+                    (\n -> unexpected $ "duplicate bindings in pattern for: " ++ n)
 
-declClauses :: (Monad m, TokenParsing m) => m [Either (String, Ann) (String, [PP], Tm)]
+
+declClauses :: (Monad m, TokenParsing m)
+            => m [Either (String, Ann) (String, Binder String [Pat Ann], Tm)]
 declClauses = semiSep $ (Left <$> typeDecl) <|> (Right <$> termDeclClause)
 
-decls :: (Monad m, TokenParsing m) => m ([(String, Ann)], [(String, [([PP], Tm)])])
+type TyDecl = (String, Ann)
+type TmDecl = (String, [(Binder String [Pat Ann], Tm)])
+
+decls :: (Monad m, TokenParsing m) => m ([TyDecl], [TmDecl])
 decls = do (ts, cs) <- partitionEithers <$> declClauses
-           fmap (ts,) . mapM validate $ groupBy ((==) `on` fst3) cs
+           fmap (ts,) . mapM validate $ groupBy ((==) `on` (^._1)) cs
  where
- fst3 (x, _, _) = x
  tail3 (_, y, z) = (y, z)
  validate ((name, ps, b):rest)
    | all pl rest = return (name, (ps, b) : map tail3 rest)
    | otherwise   =
      fail $ "Equations for `" ++ name ++ "' have differing numbers of arguments."
-  where pl (_, qs, _) = length ps == length qs
+  where pl (_, qs, _) = length (extract ps) == length (extract qs)
+
+validateDecls :: (Monad m, TokenParsing m) => [TyDecl] -> [TmDecl] -> m ()
+validateDecls tys tms
+  | Just n <- findDuplicate tyns =
+    fail $ "Duplicate type declarations for `" ++ n ++ "'."
+  | Just n <- findDuplicate tmns =
+    fail $ "Duplicate definitions for `" ++ n ++ "'."
+  | Just n <- uncovered tmns tyns =
+    fail $ "No definition for declared value `" ++ n ++ "'."
+  | otherwise = return ()
+ where
+ tyns = map fst tys
+ tmns = map fst tms
+
+--bindings :: (Monad m, TokenParsing m) => m [Binding String String]
+--bindings = do (tys, tms) <- decls
+--              validateDecls tys tms
+--              let tmns = map fst tms
+-- where
+-- mkbody ps b
+--   | 
+
+uncovered :: Ord a => [a] -> [a] -> Maybe a
+uncovered xs ys = find (`Set.notMember` s) ys
+ where
+ s = Set.fromList xs
+
+findDuplicate :: Ord a => [a] -> Maybe a
+findDuplicate = flip evalState Set.empty . foldrM test Nothing
+ where
+ test e r = state $ \s -> if e `Set.member` s then (Just e, s) else (r, Set.insert e s)
 
