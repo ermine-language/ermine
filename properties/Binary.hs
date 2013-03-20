0851f6babd9c024bc9fd2a74e05a3ffced8ca620
Dan Doel <dan.doel@gmail.com>
2013-03-20 15:58:20 -0400

    Use Lift1 from prelude-extras for the Arbitrary1 stuff

--- properties/Binary.hs
+++ properties/Binary.hs
@@ -20,6 +20,7 @@ import Data.ByteString
 import Ermine.Syntax
 import Ermine.Syntax.Global
 import Ermine.Syntax.Kind
+import Prelude.Extras
 import Test.QuickCheck
 import Test.QuickCheck.Function
 import Test.QuickCheck.Instances
@@ -69,13 +70,11 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Var a b) where
 
 class Arbitrary1 f where arbitrary1 :: Arbitrary a => Gen (f a) 
 
-newtype WrapArb f u = WrapArb { unwrapArb :: f u }
-
-instance (Arbitrary1 f, Arbitrary u) => Arbitrary (WrapArb f u) where
-    arbitrary = WrapArb <$> arbitrary1
+instance (Arbitrary1 f, Arbitrary u) => Arbitrary (Lift1 f u) where
+    arbitrary = Lift1 <$> arbitrary1
 
 instance (Arbitrary b,Arbitrary v,Arbitrary1 f,Functor f) => Arbitrary (Scope b f v) where
-    arbitrary = Scope . fmap (fmap unwrapArb) <$> arbitrary1
+    arbitrary = Scope . fmap (fmap lower1) <$> arbitrary1
 
 tests = $testGroupGenerator
 
