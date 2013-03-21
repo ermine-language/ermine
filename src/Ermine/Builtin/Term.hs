4d3f09e88621075e3d0849d18e38c5acc0eb0c86
Dan Doel <dan.doel@gmail.com>
2013-03-20 22:57:25 -0400

    Rework let/where machinery into Builtin

--- src/Ermine/Builtin/Term.hs
+++ src/Ermine/Builtin/Term.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE PatternGuards #-}
 
 --------------------------------------------------------------------
 -- |
@@ -14,12 +15,26 @@
 module Ermine.Builtin.Term ( lam
                            , dataCon
                            , let_
+                           , implicit
+                           , explicit
+                           , bindings
+                           , PreBinding(..)
+                           , finalizeBindings
+                           , finalizeBinding
+                           , PreBody(..)
+                           , body
+                           , shapely
+                           , finalizeBody
                            ) where
 
 import Bound
-import Data.ByteString.Char8
+import Control.Applicative
+import Control.Comonad
+import Data.ByteString.Char8 hiding (elemIndex, unzip, all, length)
 import Data.List as List
+import Data.Monoid
 import Ermine.Builtin.Pat
+import Ermine.Diagnostic
 import Ermine.Syntax.Global
 import Ermine.Syntax.Pat
 import Ermine.Syntax.Term
@@ -32,4 +47,42 @@ dataCon :: Fixity -> String -> String -> Term t v
 dataCon f m n = HardTerm . DataCon $ global f (pack "ermine") (pack m) (pack n)
 
 let_ :: Eq v => Binder v [Binding t v] -> Term t v -> Term t v
-let_ (Binder vs ds) body = Let ds $ abstract (`List.elemIndex` vs) body
+let_ (Binder vs ds) b = Let ds $ abstract (`List.elemIndex` vs) b
+
+data PreBinding t v = PreBinding Rendering (BindingType t) [PreBody t v]
+
+data PreBody t v = PreBody (Binder v [Pat t]) (Term t v) (Binder v [Binding t v])
+
+body :: Binder v [Pat t] -> Term t v -> Binder v [Binding t v] -> PreBody t v
+body = PreBody
+
+shapely :: [PreBody t v] -> Bool
+shapely [    ] = False
+shapely (b:bs) = all (\b' -> plength b == plength b') bs
+  where plength (PreBody ps _ _) = length $ extract ps
+
+implicit :: [PreBody t v] -> PreBinding t v
+implicit bs = PreBinding mempty Implicit bs
+
+explicit :: t -> [PreBody t v] -> PreBinding t v
+explicit t bs = PreBinding mempty (Explicit t) bs
+
+bindings :: Eq v => [(v, PreBinding t v)] -> Binder v [Binding t v]
+bindings = extend finalizeBindings . uncurry Binder . unzip
+
+finalizeBindings :: Eq v => Binder v [PreBinding t v] -> [Binding t v]
+finalizeBindings (Binder vs pbs) = finalizeBinding vs <$> pbs
+
+finalizeBinding :: Eq v => [v] -> PreBinding t v -> Binding t v
+finalizeBinding vs (PreBinding r bt bs) = Binding r bt $ finalizeBody vs <$> bs
+
+finalizeBody :: Eq v => [v] -> PreBody t v -> Body t v
+finalizeBody ns (PreBody (Binder vs ps) c (Binder ws wh)) =
+  Body ps (abstract f c) (fmap av <$> wh)
+ where
+ av x | Just i <- elemIndex x vs = B i
+      | otherwise                = F x
+ f x | Just i <- elemIndex x ws = Just (W i)
+     | Just i <- elemIndex x vs = Just (P i)
+     | Just i <- elemIndex x ns = Just (D i)
+     | otherwise                = Nothing
--- src/Ermine/Parser/Term.hs
+++ src/Ermine/Parser/Term.hs
@@ -19,15 +19,13 @@ module Ermine.Parser.Term ( anyType
                           , letBlock
                           ) where
 
-import Bound
 import Control.Lens hiding (op)
 import Control.Applicative
 import Control.Comonad
 import Control.Monad.State
 import Data.Function
 import Data.Either (partitionEithers)
-import Data.List (groupBy, find, elemIndex)
-import Data.Monoid
+import Data.List (groupBy, find)
 import Data.Set as Set hiding (map)
 import Data.Foldable (foldrM)
 import Data.Traversable hiding (mapM)
@@ -61,9 +59,9 @@ sig = (maybe id (Sig ??) ??) <$> term1 <*> optional (colon *> annotation)
 branch :: (Monad m, TokenParsing m) => m (Alt Ann (Term Ann) String)
 branch = do pp <- pat
             reserve op "->"
-            body <- term
+            b <- term
             validate pp $ \n -> unexpected $ "duplicate bindings in pattern for: " ++ n
-            return $ alt pp body
+            return $ alt pp b
 
 match :: (Monad m, TokenParsing m) => m Tm
 match = Case <$ symbol "case" <*> term <* symbol "of" <*> braces (semiSep branch)
@@ -71,13 +69,13 @@ match = Case <$ symbol "case" <*> term <* symbol "of" <*> braces (semiSep branch
 term2 :: (Monad m, TokenParsing m) => m Tm
 term2 = lambda <|> sig
 
-bindings :: (Monad m, TokenParsing m) => m (Binder String [Pat Ann])
-bindings = do pps <- sequenceA <$> some pat
+patterns :: (Monad m, TokenParsing m) => m (Binder String [Pat Ann])
+patterns = do pps <- sequenceA <$> some pat
               validate pps $ \n -> unexpected $ "duplicate bindings in pattern for: " ++ n
               return pps
 
 lambda :: (Monad m, TokenParsing m) => m Tm
-lambda = lam <$> try (bindings <* reserve op "->") <*> term
+lambda = lam <$> try (patterns <* reserve op "->") <*> term
 
 literal :: (Monad m, TokenParsing m) => m Tm
 literal = HardTerm . Lit <$>
@@ -94,41 +92,39 @@ letBlock = let_ <$ symbol "let" <*> braces declarations <* symbol "in" <*> term
 terms :: (Monad m, TokenParsing m) => m [Tm]
 terms = commaSep term
 
-typeDecl :: (Monad m, TokenParsing m) => m (String, Ann)
+typeDecl :: (Monad m, TokenParsing m) => m TyDecl
 typeDecl = (,) <$> try (ident termIdent <* colon) <*> annotation
 
 termDeclClause :: (Monad m, TokenParsing m)
-               => m (String, Binder String [Pat Ann], Tm, Where)
+               => m (String, PBody)
 termDeclClause =
-    (,,,) <$> ident termIdent <*> pat0s <* reserve op "=" <*> term <*> whereClause
+    (,) <$> ident termIdent
+        <*> (body <$> pat0s <* reserve op "=" <*> term <*> whereClause)
  where
  pat0s = do ps <- sequenceA <$> many pat0
             ps <$ validate ps
                     (\n -> unexpected $ "duplicate bindings in pattern for: " ++ n)
 
+type PBody = PreBody Ann String
 type Where = Binder String [Binding Ann String]
 
 whereClause :: (Monad m, TokenParsing m) => m Where
 whereClause = symbol "where" *> braces declarations <|> pure (pure [])
 
-declClauses :: (Monad m, TokenParsing m)
-            => m [Either (String, Ann) (String, Binder String [Pat Ann], Tm, Where)]
+declClauses :: (Monad m, TokenParsing m) => m [Either TyDecl (String, PBody)]
 declClauses = semiSep $ (Left <$> typeDecl) <|> (Right <$> termDeclClause)
 
 type TyDecl = (String, Ann)
-type TmDecl = (String, [(Binder String [Pat Ann], Tm, Where)])
+type TmDecl = (String, [PBody])
 
 decls :: (Monad m, TokenParsing m) => m ([TyDecl], [TmDecl])
 decls = do (ts, cs) <- partitionEithers <$> declClauses
            fmap (ts,) . mapM validateShape $ groupBy ((==) `on` (^._1)) cs
  where
- tail4 (_, x, y, z) = (x, y, z)
- validateShape [                  ] = error "decl:validateShape: IMPOSSIBLE"
- validateShape ((name, ps, b, wh):rest)
-   | all pl rest = return (name, (ps, b, wh) : map tail4 rest)
-   | otherwise   =
-     fail $ "Equations for `" ++ name ++ "' have differing numbers of arguments."
-  where pl (_, qs, _, _) = length (extract ps) == length (extract qs)
+ validateShape l = let (name:_, pbs) = unzip l in
+   if shapely pbs
+     then return (name, pbs)
+     else fail $ "Equations for `" ++ name ++ "' have differing numbers of arguments."
 
 validateDecls :: (Monad m, TokenParsing m) => [TyDecl] -> [TmDecl] -> m ()
 validateDecls tys tms
@@ -146,21 +142,11 @@ validateDecls tys tms
 declarations :: (Monad m, TokenParsing m) => m (Binder String [Binding Ann String])
 declarations = do
   (tys, tms) <- decls
-  let ns = map fst tms
-      mkbody (Binder vs ps, b, wh) = Body ps (abstract f b) (fmap av <$> extract wh)
-       where ws = vars wh
-             av x | Just i <- elemIndex x vs = B i
-                  | otherwise                = F x
-             f x | Just i <- elemIndex x ws = Just (W i)
-                 | Just i <- elemIndex x vs = Just (P i)
-                 | Just i <- elemIndex x ns = Just (D i)
-                 | otherwise                = Nothing
-      -- TODO: Rendering
-      mkbinding (s, pbs) = Binding mempty (bindType s) $ map mkbody pbs
+  let -- TODO: Rendering
       bindType s
-        | Just t <- lookup s tys = Explicit t
-        | otherwise              = Implicit
-  Binder ns (map mkbinding tms) <$ validateDecls tys tms
+        | Just t <- lookup s tys = explicit t
+        | otherwise              = implicit
+  bindings (extend (uncurry bindType) <$> tms) <$ validateDecls tys tms
 
 uncovered :: Ord a => [a] -> [a] -> Maybe a
 uncovered xs ys = find (`Set.notMember` s) ys
