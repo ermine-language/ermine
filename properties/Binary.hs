20926d383403695d7219dbb4e4306151ed52a2d1
joshcough <joshcough@gmail.com>
2013-03-22 12:01:20 -0400

    added pack/unpack for hard type. and for type, but it runs out of mem. and cleaned up a bit

--- properties/Binary.hs
+++ properties/Binary.hs
@@ -19,7 +19,8 @@ import Data.Binary.Put
 import Data.ByteString
 import Ermine.Syntax
 import Ermine.Syntax.Global
-import Ermine.Syntax.Kind
+import Ermine.Syntax.Kind as K
+import Ermine.Syntax.Type as T
 import Prelude.Extras
 import Test.QuickCheck
 import Test.QuickCheck.Function
@@ -36,15 +37,15 @@ genPrecedence = choose (0, 9) :: Gen Int
 
 instance Arbitrary Fixity where
     arbitrary =
-      oneof [ liftM2 Infix   arbitrary genPrecedence,
-              liftM  Prefix  genPrecedence,
-              liftM  Postfix genPrecedence,
-              return Idfix ]
+      oneof [ Infix   <$> arbitrary <*> genPrecedence,
+              Prefix  <$> genPrecedence,
+              Postfix <$> genPrecedence,
+              return  Idfix ]
 
 prop_pack_unpack_fixity f = (unpackFixity . packFixity) f == f
 
 instance Arbitrary Global where
-    arbitrary = liftM4 global arbitrary arbitrary arbitrary arbitrary
+    arbitrary = global <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
 
 pack_unpack :: (Binary a, Eq a) => a -> Bool
 pack_unpack a = runGet get (runPut $ put a) == a
@@ -53,16 +54,16 @@ prop_pack_unpack_global :: Global -> Bool
 prop_pack_unpack_global = pack_unpack
 
 instance Arbitrary HardKind where
-    arbitrary = oneof $ fmap return [ Star, Constraint, Rho, Phi ]
+    arbitrary = oneof $ return <$> [ Star, Constraint, Rho, Phi ]
 
 prop_pack_unpack_hardkind :: HardKind -> Bool
 prop_pack_unpack_hardkind = pack_unpack
 
 instance Arbitrary a => Arbitrary (Kind a) where
     arbitrary =
-      oneof [ liftM  Var arbitrary,
-              liftM2 (:->) arbitrary arbitrary,
-              liftM  HardKind arbitrary ]
+      oneof [ K.Var    <$> arbitrary,
+              (:->)    <$> arbitrary <*> arbitrary,
+              HardKind <$> arbitrary ]
 
 instance Arbitrary1 Kind where
     arbitrary1 = arbitrary
@@ -71,7 +72,7 @@ prop_pack_unpack_kind :: Kind Int -> Bool
 prop_pack_unpack_kind = pack_unpack
 
 instance (Arbitrary a, Arbitrary b) => Arbitrary (Var a b) where
-    arbitrary = oneof [ liftM B arbitrary, liftM F arbitrary ]
+    arbitrary = oneof [ B <$> arbitrary, F <$> arbitrary ]
 
 instance (Arbitrary1 f, Arbitrary u) => Arbitrary (Lift1 f u) where
     arbitrary = Lift1 <$> arbitrary1
@@ -84,6 +85,36 @@ instance Arbitrary a => Arbitrary (Schema a) where
 
 prop_pack_unpack_schema :: Schema Int -> Bool
 prop_pack_unpack_schema = pack_unpack
+
+instance Arbitrary T.HardType where
+    arbitrary = 
+      oneof [ T.Tuple  <$> arbitrary,
+              return T.Arrow,
+              -- TODO: learn more about Data.Void
+              -- | Con !Global !(Schema Void)
+              --T.Con    <$> arbitrary <*> arbitrary,
+              T.ConcreteRho <$> arbitrary ]
+
+instance Arbitrary k => Arbitrary1 (Type k) where
+    arbitrary1 = arbitrary 
+
+instance (Arbitrary k, Arbitrary a) => Arbitrary (Type k a) where
+    arbitrary =
+      oneof [ T.Var    <$> arbitrary
+             ,App      <$> arbitrary <*> arbitrary
+             ,HardType <$> arbitrary
+             ,Forall   <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+             -- | Loc !Rendering !(Type k a)
+             --,Loc something something
+             ,Exists   <$> arbitrary <*> arbitrary <*> arbitrary
+             ,And      <$> arbitrary
+            ]
+
+prop_pack_unpack_hardtype :: HardType -> Bool
+prop_pack_unpack_hardtype = pack_unpack
+
+--prop_pack_unpack_type :: Type Int Int -> Bool
+--prop_pack_unpack_type = pack_unpack
 
 tests = $testGroupGenerator
 
