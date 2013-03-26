dd5815179ee95626fa21529b8415657582ec4dcc
Dan Doel <dan.doel@gmail.com>
2013-03-25 23:03:18 -0400

    Printing for Globals, Literals and start of Term

---
+++ src/Ermine/Pretty/Literal.hs
@@ -0,0 +1,25 @@
+
+--------------------------------------------------------------------
+-- |
+-- Copyright :  (c) Dan Doel 2013
+-- License   :  BSD3
+-- Maintainer:  Dan Doel <dan.doel@gmail.com>
+-- Stability :  experimental
+-- Portability: non-portable
+--
+--------------------------------------------------------------------
+
+module Ermine.Pretty.Literal (prettyLiteral) where
+
+import Ermine.Pretty
+import Ermine.Syntax.Literal
+
+prettyLiteral :: Literal -> Doc
+prettyLiteral (Int i)    = text $ show i
+prettyLiteral (Int64 l)  = text $ show l ++ "L"
+prettyLiteral (Byte b)   = text $ show b ++ "B"
+prettyLiteral (Short s)  = text $ show s ++ "S"
+prettyLiteral (String s) = text $ show s
+prettyLiteral (Char c)   = text $ show c
+prettyLiteral (Float f)  = text $ show f ++ "F"
+prettyLiteral (Double d) = text $ show d
