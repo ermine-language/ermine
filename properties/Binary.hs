9c5992bbdcc5c9847fd71bc594c94db0b4b5a677
Josh Cough <joshcough@gmail.com>
2013-03-27 12:34:12 -0400

    fixed indentation.

--- properties/Binary.hs
+++ properties/Binary.hs
@@ -34,21 +34,21 @@ import Test.Framework.Providers.QuickCheck2
 class Arbitrary1 f where arbitrary1 :: Arbitrary a => Gen (f a)
 
 instance Arbitrary Assoc where
-  arbitrary     = Test.QuickCheck.elements [L, R, N]
+  arbitrary = Test.QuickCheck.elements [L, R, N]
 
 genPrecedence = choose (0, 9) :: Gen Int
 
 instance Arbitrary Fixity where
-    arbitrary =
-      oneof [ Infix   <$> arbitrary <*> genPrecedence,
-              Prefix  <$> genPrecedence,
-              Postfix <$> genPrecedence,
-              return  Idfix ]
+  arbitrary = oneof [
+    Infix   <$> arbitrary <*> genPrecedence,
+    Prefix  <$> genPrecedence,
+    Postfix <$> genPrecedence,
+    return  Idfix ]
 
 prop_pack_unpack_fixity f = (unpackFixity . packFixity) f == Just f
 
 instance Arbitrary Global where
-    arbitrary = glob <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+  arbitrary = glob <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
 
 pack_unpack :: (Binary a, Eq a) => a -> Bool
 pack_unpack a = runGet get (runPut $ put a) == a
@@ -57,46 +57,46 @@ prop_pack_unpack_global :: Global -> Bool
 prop_pack_unpack_global = pack_unpack
 
 instance Arbitrary HardKind where
-    arbitrary = oneof $ return <$> [ Star, Constraint, Rho, Phi ]
+  arbitrary = oneof $ return <$> [ Star, Constraint, Rho, Phi ]
 
 prop_pack_unpack_hardkind :: HardKind -> Bool
 prop_pack_unpack_hardkind = pack_unpack
 
 instance Arbitrary a => Arbitrary (Kind a) where
-    arbitrary =
-      oneof [ K.Var    <$> arbitrary,
-              (:->)    <$> arbitrary <*> arbitrary,
-              HardKind <$> arbitrary ]
+  arbitrary = oneof [
+    K.Var    <$> arbitrary,
+    (:->)    <$> arbitrary <*> arbitrary,
+    HardKind <$> arbitrary ]
 
 instance Arbitrary1 Kind where
-    arbitrary1 = arbitrary
+  arbitrary1 = arbitrary
 
 prop_pack_unpack_kind :: Kind Int -> Bool
 prop_pack_unpack_kind = pack_unpack
 
 instance (Arbitrary a, Arbitrary b) => Arbitrary (Var a b) where
-    arbitrary = oneof [ B <$> arbitrary, F <$> arbitrary ]
+  arbitrary = oneof [ B <$> arbitrary, F <$> arbitrary ]
 
 instance (Arbitrary1 f, Arbitrary u) => Arbitrary (Lift1 f u) where
-    arbitrary = Lift1 <$> arbitrary1
+  arbitrary = Lift1 <$> arbitrary1
 
 instance (Arbitrary b,Arbitrary v,Arbitrary1 f,Functor f) => Arbitrary (Scope b f v) where
-    arbitrary = Scope . fmap (fmap lower1) <$> arbitrary1
+  arbitrary = Scope . fmap (fmap lower1) <$> arbitrary1
 
 instance Arbitrary a => Arbitrary (Schema a) where
-    arbitrary = Schema <$> arbitrary <*> arbitrary
+  arbitrary = Schema <$> arbitrary <*> arbitrary
 
 prop_pack_unpack_schema :: Schema Int -> Bool
 prop_pack_unpack_schema = pack_unpack
 
 instance Arbitrary Type.HardType where
-    arbitrary = 
-      oneof [ Type.Tuple  <$> arbitrary,
-              return Type.Arrow,
-              -- TODO: learn more about Data.Void
-              -- | Con !Global !(Schema Void)
-              --Type.Con    <$> arbitrary <*> arbitrary,
-              Type.ConcreteRho <$> arbitrary ]
+  arbitrary = oneof [
+    Type.Tuple  <$> arbitrary,
+    return Type.Arrow,
+    -- TODO: learn more about Data.Void
+    -- | Con !Global !(Schema Void)
+    --Type.Con    <$> arbitrary <*> arbitrary,
+    Type.ConcreteRho <$> arbitrary ]
 
 instance Arbitrary k => Arbitrary1 (Type k) where
     arbitrary1 = arbitrary 
@@ -107,18 +107,17 @@ resizearb :: Arbitrary a => Int -> Gen a
 resizearb n = resize (n `div` 2) arbitrary
 
 instance (Arbitrary k, Arbitrary a) => Arbitrary (Type k a) where
-    arbitrary = sized type' where 
-      type' 0 = oneof [ Type.Var <$> arbitrary, HardType <$> arbitrary ]
-      type' n | n>0 = 
-        oneof [ Type.Var    <$> arbitrary
-               ,Type.App      <$> resizearb n <*> resizearb n
-               ,HardType <$> arbitrary
-               ,Forall   <$> arbitrary <*> resizearb n <*> resizearb n <*> resizearb n
-               -- | Loc !Rendering !(Type k a)
-               --,Loc something something
-               ,Exists   <$> arbitrary <*> resizearb n <*> resizearb n
-               ,And      <$> resizearb n
-              ]
+  arbitrary = sized type' where
+    type' 0 = oneof [ Type.Var <$> arbitrary, HardType <$> arbitrary ]
+    type' n | n>0 = oneof [
+      Type.Var    <$> arbitrary
+     ,Type.App      <$> resizearb n <*> resizearb n
+     ,HardType <$> arbitrary
+     ,Forall   <$> arbitrary <*> resizearb n <*> resizearb n <*> resizearb n
+     -- | Loc !Rendering !(Type k a)
+     --,Loc something something
+     ,Exists   <$> arbitrary <*> resizearb n <*> resizearb n
+     ,And      <$> resizearb n ]
 
 prop_pack_unpack_hardtype :: HardType -> Bool
 prop_pack_unpack_hardtype = pack_unpack
@@ -128,15 +127,15 @@ prop_pack_unpack_type = pack_unpack
 
 
 instance Arbitrary Literal where
-    arbitrary =
-      oneof [ Int     <$> arbitrary,
-              Int64   <$> arbitrary,
-              Byte    <$> arbitrary,
-              Short   <$> arbitrary,
-              String  <$> arbitrary,
-              Char    <$> arbitrary,
-              Float   <$> arbitrary,
-              Double  <$> arbitrary ]
+  arbitrary = oneof [
+    Int     <$> arbitrary,
+    Int64   <$> arbitrary,
+    Byte    <$> arbitrary,
+    Short   <$> arbitrary,
+    String  <$> arbitrary,
+    Char    <$> arbitrary,
+    Float   <$> arbitrary,
+    Double  <$> arbitrary ]
 
 prop_pack_unpack_literal :: Literal -> Bool
 prop_pack_unpack_literal = pack_unpack
