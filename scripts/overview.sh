0b949416b1fc4dbbd22b57d1af027bd8c121f77e
Edward Kmett <ekmett@gmail.com>
2013-03-27 18:56:04 -0400

    collapse Ermine.Binary

--- scripts/overview.sh
+++ scripts/overview.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-find src -name "*.hs" -print | xargs graphmod --no-cluster -C Ermine.Syntax -C Ermine.Pretty -C Ermine.Inference -C Ermine.Builtin -C Ermine.Unification -C Ermine.Parser -C Ermine.Console | dot -Tpng > images/overview.png
+find src -name "*.hs" -print | xargs graphmod --no-cluster -C Ermine.Syntax -C Ermine.Pretty -C Ermine.Inference -C Ermine.Builtin -C Ermine.Unification -C Ermine.Parser -C Ermine.Console -C Ermine.Binary | dot -Tpng > images/overview.png
