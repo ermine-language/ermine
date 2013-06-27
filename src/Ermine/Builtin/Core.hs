94afa8fda0783cd967d62317eb2710f5064927c7
Dan Doel <dan.doel@gmail.com>
2013-06-26 22:57:44 -0400

    Implemented a core combinator for branching definitions.

--- src/Ermine/Builtin/Core.hs
+++ src/Ermine/Builtin/Core.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 
 --------------------------------------------------------------------
 -- |
@@ -11,12 +12,14 @@
 
 module Ermine.Builtin.Core
   ( plam
+  , plamBranch
   ) where
 
 import Bound
 import Control.Applicative
 import Control.Lens
 import Control.Comonad
+import Data.List (transpose)
 import qualified Data.HashMap.Lazy as HM
 import Ermine.Builtin.Pattern
 import Ermine.Syntax.Core
@@ -33,3 +36,17 @@ plam ps body = Lam n . Scope <$> compile ci pm
               [Trivial]
               [F . pure <$> abstract (`lookup` assocs) body]
  ci = CInfo HM.empty (map (pure . B) [0..n-1]) (map argPP [0..n-1])
+
+plamBranch :: (Eq v, MonadPComp m) => [([P t v], [(Maybe (Core v), Core v)])] -> m (Core v)
+plamBranch bs | null bs = pure . HardCore $ Error "Empty lambda branch"
+              | any ((/= n) . length) ps = pure . HardCore $ Error "Non-uniform patterns"
+              | otherwise = Lam n . Scope <$> compile ci pm
+ where (bps, gs, cs) = unzip3 $ bs >>= \(bp, gcs) -> map (uncurry (bp,,)) gcs
+       n  = length $ head ps
+       ps = map (map extract) bps
+       is = map (iconcatMap (\i (Binder vs p) -> zip vs . map (ArgPP i) $ paths p)) bps
+       mkguard _ Nothing  = Trivial
+       mkguard i (Just c) = Explicit $ F . pure <$> abstract (`lookup` i) c
+       pm = PMatrix (transpose ps) (zipWith mkguard is gs)
+                    (zipWith (\i c -> F . pure <$> abstract (`lookup` i) c) is cs)
+       ci = CInfo HM.empty (map (pure . B) [0..n-1]) (map argPP [0..n-1])
