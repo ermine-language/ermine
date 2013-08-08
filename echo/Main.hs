6ce9b510a20ec92f13f6a6541b1182ab301cd3ee
Josh Cough <joshcough@gmail.com>
2013-08-08 12:32:04 -0400

    core round trip serialization testing.

--- echo/Main.hs
+++ echo/Main.hs
@@ -1,56 +1,18 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main ( main ) where
 
-import Control.Applicative
-import Control.Exception.Lens
-import Control.Lens
-import Control.Monad.State.Strict
-import Data.ByteString.Lens
-import Data.Char
-import Data.Default
-import Data.Monoid
-import Ermine
-import Ermine.Console.State
-import Ermine.Console.Command
-import Ermine.Console.Completion
-import Ermine.Console.Options
-import Ermine.Console.Unicode
--- import Ermine.Version
-import Options.Applicative
-import System.Console.Haskeline
-import System.Exit.Lens
-import System.Remote.Monitoring
+import qualified Data.Binary as Binary
+import qualified Data.ByteString.Lazy as B
+import Ermine.Syntax.Core
 
 main :: IO ()
-main = withUnicode $ do
-  optionParser <- parseOptions
+main = do
+       input <- B.readFile "core.in"
+       let r = writeCore $ readCore input
+       B.writeFile "core.out" r
 
-  opts <- execParser $ info (helper <*> optionParser) $
-    fullDesc
-    <> progDesc "The Ermine Programming Language"
-    <> header ("Ermine " ++ version)
+readCore :: B.ByteString -> Core Int
+readCore = Binary.decode
 
-  putStrLn logo
-
-  server <- if opts^.monitorEnabled
-    then do
-      putStrLn $ "Monitoring enabled at http://" ++ opts^.monitorHost ++ ":" ++ show (opts^.monitorPort) ++ "/"
-      Just <$> forkServer (opts^.monitorHost.packedChars) (opts^.monitorPort)
-    else pure Nothing
-
-  evalStateT ?? def $ runInputT settings $ do
-    loop server
-
-loop :: Maybe Server -> InputT Console ()
-loop server = do
-  minput <- getInputLine ">> "
-  case Prelude.dropWhile isSpace <$> minput of
-    Nothing      -> return ()
-    Just "quit"  -> return ()
-    Just (':':cmd) -> do
-      lift $ handling (filtered (hasn't _ExitCode)) (liftIO . print) (executeCommand cmd)
-      loop server
-    Just ""      -> loop server
-    Just input   -> do
-      outputStrLn $ "Input was: " ++ input
-      loop server
+writeCore :: Core Int -> B.ByteString
+writeCore = Binary.encode
