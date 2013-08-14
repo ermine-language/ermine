04b7fc8550ab87b56f9a58d6520183ae09ba489e
Josh Cough <joshcough@gmail.com>
2013-08-14 16:19:18 -0400

    changed from HardCore to (Core Int)

--- echo/Main.hs
+++ echo/Main.hs
@@ -4,6 +4,7 @@ module Main ( main ) where
 import Control.Exception (catch, SomeException)
 import qualified Data.Binary as Binary
 import qualified Data.ByteString.Lazy as B
+import Data.Int
 import Data.List (intersperse)
 import Data.Monoid (mappend, mconcat)
 import Data.Text as SText hiding (intersperse, map)
@@ -14,28 +15,15 @@ main :: IO ()
 main = do
        input <- B.readFile "core.in"
        putStrLn . showbytes $ input
-       --putStrLn . showbytes . writeCore $ (Lit . Lit.Char $ 'A')
-       --putStrLn . showbytes . writeCore $ (Lit . Lit.String $ SText.pack "A")
+       putStrLn . showbytes $ writeCore $ readCore input
        let r = B.writeFile "core.out" . writeCore $ readCore input
        catch r (writeFile "failure.txt" . mappend "failed: " . (show :: SomeException -> String))
 
-readCore :: B.ByteString -> HardCore
+readCore :: B.ByteString -> Core Int32
 readCore = Binary.decode
 
 showbytes :: B.ByteString -> String
 showbytes = mconcat . ("Bytes: ":) . intersperse ", " . map show . B.unpack
 
-writeCore :: HardCore -> B.ByteString
+writeCore :: Core Int32 -> B.ByteString
 writeCore = Binary.encode
-
-{-
-main :: IO ()
-main = do
-       input <- B.getContents
-       let l = B.length input
-       l `seq` return ()
-       writeFile "echoBytesIn.txt" $ mconcat ["Read ", show l, " bytes\n"]
-       let out = writeCore $ readCore input
-       writeFile "echoBytesOut.txt" $ mconcat ["Read ", show l, " bytes, wrote ", show (B.length out), " bytes.\n"]
-       B.putStr out
--}
