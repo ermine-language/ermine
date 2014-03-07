b3e336c88b18ac37bbf1011b50bca2b73fac0acb
Dan Doel <dan.doel@gmail.com>
2014-03-06 22:35:31 -0500

    Removed CPP from Monitor

--- src-opt/no-ekg/Ermine/Monitor.hs
+++ src-opt/no-ekg/Ermine/Monitor.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE CPP #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
@@ -26,32 +25,19 @@ module Ermine.Monitor
   , Incremental(..)
   , dec
   , sub
-#ifdef EKG
-  -- * Compatibilty with EKG
-  , withServer
-#endif
+-- #ifdef EKG
+--   -- * Compatibilty with EKG
+--   , withServer
+-- #endif
   ) where
 
 import Control.Exception
 import Control.Lens hiding (Setting)
 import Control.Monad.Trans
 import Control.Monad.Reader
-import Data.ByteString.Lens
 import Data.Data
 import Data.Text
-#ifndef EKG
-import Data.Void
-#endif
 import Options.Applicative
-import System.Process
-#ifdef EKG
-import System.Remote.Monitoring
-import qualified System.Remote.Gauge as G
-import qualified System.Remote.Counter as C
-import qualified System.Remote.Label as L
-#endif
-
--- | Enable/disable EKG
 
 data MonitorOptions = MonitorOptions
   { _monitorHost    :: String
@@ -67,15 +53,7 @@ monitorUri t = "http://" ++ t^.monitorHost ++ ":" ++ show (t^.monitorPort) ++ "/
 
 -- | Parse EKG configuration
 parseMonitorOptions :: Parser MonitorOptions
-#ifdef EKG
-parseMonitorOptions = MonitorOptions
-  <$> strOption (long "ekg-host" <> short 'H' <> help "host for the EKG server" <> metavar "HOST" <> action "hostname" <> value "localhost")
-  <*> option (long "ekg-port" <> short 'P' <> help "port for the EKG server" <> metavar "PORT" <> value 5616)
-  <*> (not <$> switch (long "no-ekg" <> short 'Q' <> help "do NOT start the EKG server" <> value False))
-  <*> switch (long "ekg-open" <> short 'M' <> help "open EKG on launch")
-#else
-parseMonitorOptions = return $ MonitorOptions "localhost" 5616 False False
-#endif
+parseMonitorOptions = pure $ MonitorOptions "localhost" 5616 False False
 
 data ShutdownMonitor = ShutdownMonitor deriving (Typeable, Show)
 
@@ -83,9 +61,6 @@ instance Exception ShutdownMonitor
 
 data Monitor = Monitor
   { __monitorOptions :: MonitorOptions
-#ifdef EKG
-  , _monitorServer   :: Maybe Server
-#endif
   }
 
 makeClassy ''Monitor
@@ -93,12 +68,12 @@ makeClassy ''Monitor
 instance HasMonitorOptions Monitor where
   monitorOptions = _monitorOptions
 
-#ifdef EKG
-withServer :: HasMonitor t => t -> (Server -> IO ()) -> IO ()
-withServer t k = case t^.monitorServer of
-  Nothing -> return ()
-  Just s  -> k s
-#endif
+-- #ifdef EKG
+-- withServer :: HasMonitor t => t -> (Server -> IO ()) -> IO ()
+-- withServer t k = case t^.monitorServer of
+--   Nothing -> return ()
+--   Just s  -> k s
+-- #endif
 
 class Setting t a | t -> a where
   assign :: MonadIO m => t -> a -> m ()        -- set
@@ -106,30 +81,12 @@ class Setting t a | t -> a where
   update :: MonadIO m => t -> (a -> a) -> m () -- modify
   update _ _ = return ()
 
-#ifdef EKG
-
-newtype Gauge = Gauge { runGauge :: Maybe G.Gauge }
-newtype Label = Label { runLabel :: Maybe L.Label }
-newtype Counter = Counter { runCounter :: Maybe C.Counter }
-
-instance Setting Label Text where
-  assign (Label t) a = liftIO $ maybe (return ()) (L.set ?? a) t
-  update (Label t) f = liftIO $ maybe (return ()) (L.modify f) t
-
-instance Setting Gauge Int where
-  assign (Gauge t) a = liftIO $ maybe (return ()) (G.set ?? a) t
-  update (Gauge t) f = liftIO $ maybe (return ()) (G.modify f) t
-
-#else
-
 data Gauge = Gauge Text
 data Label = Label Text
 data Counter = Counter Text
 
 instance Setting Label Text
 instance Setting Gauge Int
-
-#endif
 
 dec :: (MonadIO m, Setting t a, Num a) => t -> m ()
 dec t = update t (subtract 1)
@@ -145,16 +102,7 @@ class Incremental t where
   add _ _ = return ()
 
 instance Incremental Gauge where
-#ifdef EKG
-  inc (Gauge t)   = liftIO $ maybe (return ()) G.inc t
-  add (Gauge t) i = liftIO $ maybe (return ()) (G.add ?? i) t
-#endif
-
 instance Incremental Counter where
-#ifdef EKG
-  inc (Counter t)   = liftIO $ maybe (return ()) C.inc t
-  add (Counter t) i = liftIO $ maybe (return ()) (C.add ?? i) t
-#endif
 
 gauge :: (MonadIO m, HasMonitor t) => Text -> t -> m Gauge
 gauge = runReaderT . gaugeM
@@ -167,38 +115,15 @@ label = runReaderT . labelM
 
 -- | create a gauge
 gaugeM :: (MonadIO m, MonadReader t m, HasMonitor t) => Text -> m Gauge
-#ifdef EKG
-gaugeM l = view monitorServer >>= maybe (return $ Gauge Nothing) (liftIO . fmap (Gauge . Just) . getGauge l)
-#else
 gaugeM = return . Gauge
-#endif
 
 -- | create a counter
 counterM :: (MonadIO m, MonadReader t m, HasMonitor t) => Text -> m Counter
-#ifdef EKG
-counterM l = view monitorServer >>= maybe (return $ Counter Nothing) (liftIO . fmap (Counter . Just) . getCounter l)
-#else
 counterM = return . Counter
-#endif
 
 -- | create a label
 labelM :: (MonadIO m, MonadReader t m, HasMonitor t) => Text -> m Label
-#ifdef EKG
-labelM t = view monitorServer >>= maybe (return $ Label Nothing) (liftIO . fmap (Label . Just) . getLabel t)
-#else
 labelM = return . Label
-#endif
 
 withMonitor :: HasMonitorOptions t => t -> (Monitor -> IO a) -> IO a
-withMonitor t k
-#ifdef EKG
-  | t^.monitorEnabled = do
-    server <- forkServer (t^.monitorHost.packedChars) (t^.monitorPort)
-    let uri = monitorUri t
-    putStrLn $ "Monitoring enabled at " ++ uri
-    when (t^.monitorOpen) $ do
-      _ <- system $ "/usr/bin/open " ++ uri
-      return ()
-    k (Monitor (t^.monitorOptions) $ Just server) `finally` throwTo (serverThreadId server) ShutdownMonitor
-#endif
-  | otherwise = k $ Monitor (t^.monitorOptions) Nothing
+withMonitor t k = k $ Monitor (t^.monitorOptions)
