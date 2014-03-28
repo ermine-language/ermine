5b55603c2247a9c19f412c71ffb412f78e75fd7a
Edward Kmett <ekmett@gmail.com>
2014-03-28 01:41:32 -0400

    switch doesn't have value

--- src-opt/ekg/Ermine/Monitor.hs
+++ src-opt/ekg/Ermine/Monitor.hs
@@ -64,7 +64,7 @@ parseMonitorOptions :: Parser MonitorOptions
 parseMonitorOptions = MonitorOptions
   <$> strOption (long "ekg-host" <> short 'H' <> help "host for the EKG server" <> metavar "HOST" <> action "hostname" <> value "localhost")
   <*> option (long "ekg-port" <> short 'P' <> help "port for the EKG server" <> metavar "PORT" <> value 5616)
-  <*> (not <$> switch (long "no-ekg" <> short 'Q' <> help "do NOT start the EKG server" <> value False))
+  <*> (not <$> switch (long "no-ekg" <> short 'Q' <> help "do NOT start the EKG server"))
   <*> switch (long "ekg-open" <> short 'M' <> help "open EKG on launch")
 
 data ShutdownMonitor = ShutdownMonitor deriving (Typeable, Show)
