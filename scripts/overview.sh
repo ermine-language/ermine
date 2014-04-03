0fc831938e53e796b90586324aa6cae857a51b32
Edward Kmett <ekmett@gmail.com>
2014-04-03 09:11:28 -0400

    collapse more of the graph

--- scripts/overview.sh
+++ scripts/overview.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/sh
-find src -name "*.hs" -print | xargs graphmod --no-cluster -C Ermine.Syntax -C Ermine.Pretty -C Ermine.Inference -C Ermine.Builtin -C Ermine.Unification -C Ermine.Parser -C Ermine.Console -C Ermine.Binary -C Ermine.Pattern -C Ermine.Constraint -r Main | dot -Tpng > images/overview.png
+#!/bin/sh
+find src -name "*.hs" -print | xargs graphmod --no-cluster -C Ermine.Syntax -C Ermine.Pretty -C Ermine.Inference -C Ermine.Builtin -C Ermine.Unification -C Ermine.Parser -C Ermine.Console -C Ermine.Binary -C Ermine.Pattern -C Ermine.Constraint -C Ermine.Monitor -C Ermine.Core -r Main | dot -Tpng > images/overview.png
