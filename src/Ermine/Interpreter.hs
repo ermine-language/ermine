7eb8bcfa839a00a14b1a1c92dda9e978ed80b4a2
Dan Doel <dan.doel@gmail.com>
2014-04-06 22:17:33 -0400

    Toward a Core -> Code 'assembler'; still missing cases

--- src/Ermine/Interpreter.hs
+++ src/Ermine/Interpreter.hs
@@ -23,7 +23,7 @@ module Ermine.Interpreter
   , Value(..)
   , GEnv
   , LEnv
-  , Var(..)
+  , Ref(..)
   , Tag
   , LambdaForm(LambdaForm)
   , Func(..)
@@ -32,6 +32,8 @@ module Ermine.Interpreter
   , update
   , body
   , standardConstructor
+  , assembleBinding
+  , assemble
   , Code(..)
   , Frame(..)
   , MachineState
@@ -41,38 +43,54 @@ module Ermine.Interpreter
   , eval
   ) where
 
+import Bound
+import Bound.Var
 import Control.Applicative hiding (empty)
 import Control.Monad.Primitive
+import Control.Monad.State
 import Control.Lens
 import Data.Default
-import Data.Map hiding (null, update)
+import Data.Foldable (toList)
+import Data.List (elemIndex, nub)
+import Data.Map hiding (null, update, toList, filter)
 import Data.Maybe
 import Data.Primitive.MutVar
 import Data.Traversable
 import Data.Word
+import Ermine.Syntax.Core (Core, HardCore, Match(..))
+import qualified Ermine.Syntax.Core as Core
 import Prelude hiding (lookup)
 
 ------------------------------------------------------------------------------
 -- Syntax
 ------------------------------------------------------------------------------
 
-data Var = Global Word32 | Local Word32
+data Ref = Global Word32 | Local Word32
   deriving Show
+
+makePrisms ''Ref
 
 data Code
   = Case Code Continuation
-  | App Func [Var]
-  | Let [([Var], LambdaForm)] Code
-  | LetRec [([Var], LambdaForm)] Code
+  | App Func [Ref]
+  | Let [PreClosure] Code
+  | LetRec [PreClosure] Code
   | Lit Word64
   deriving Show
+
+_Ref :: Prism' Code Ref
+_Ref = prism (\r -> App (Ref r) []) $ \case
+  App (Ref r) [] -> Right r
+  co             -> Left co
+
+type PreClosure = ([Ref], LambdaForm)
 
 type Tag = Word8
 
 data Continuation = Cont (Map Tag Code) (Maybe Code)
   deriving Show
 
-data Func = Var Var | Con Tag
+data Func = Ref Ref | Con Tag
   deriving Show
 
 data LambdaForm = LambdaForm
@@ -82,8 +100,96 @@ data LambdaForm = LambdaForm
   , _body :: Code
   } deriving Show
 
+noUpdate :: Word32 -> Word8 -> Code -> LambdaForm
+noUpdate f b e = LambdaForm f b False e
+
+doUpdate :: Word32 -> Code -> LambdaForm
+doUpdate f e = LambdaForm f 0 True e
+
 standardConstructor :: Word32 -> Tag -> LambdaForm
 standardConstructor w t = LambdaForm w 0 False . App (Con t) $ Local <$> [0..w-1]
+
+genericLength :: Num n => [a] -> n
+genericLength = fromIntegral . length
+
+genericElemIndex :: (Eq a, Num n) => a -> [a] -> Maybe n
+genericElemIndex x = fmap fromIntegral . elemIndex x
+
+assembleBinding :: Eq v => (v -> Ref) -> Core v -> PreClosure
+assembleBinding cxt co = (,) vs $ case co of
+  -- Assumes that core lambdas are never 0-arity
+  Core.Lam ccvs body ->
+    noUpdate fvn bvn (assemble (fvn + fromIntegral bvn) cxt'' $ fromScope body)
+   where bvn  = genericLength ccvs
+         cxt'' (F v) = cxt' v
+         cxt'' (B b) = Local $ fvn + fromIntegral b
+  -- TODO: this is naive
+  _ -> doUpdate fvn (assemble fvn cxt' $ co)
+ where
+ (fvs, vs) =
+   unzip . filter (has $ _2._Local) . fmap (\v -> (v, cxt v)) . nub . toList $ co
+ fvn = genericLength vs
+
+ cxt' v | Just n <- genericElemIndex v fvs = Local n
+        | otherwise                        = cxt v
+
+assemble :: Eq v => Word32 -> (v -> Ref) -> Core v -> Code
+assemble _ cxt (Core.Var v) = App (Ref $ cxt v) []
+assemble _ _   (Core.HardCore hc) = assembleHardCore hc
+assemble n cxt (Core.App _   f x) = assembleApp n cxt [x] f
+assemble n cxt l@Core.Lam{} =
+  Let [assembleBinding cxt l] (App (Ref $ Local n) [])
+assemble n cxt (Core.Case e bs d) =
+  (if null cs then id else Let cs) . Case e' $ assembleBranches n' ev cxt bs d
+ where
+ (cs, n', ev, e') = case e of
+   Core.Var v -> ([], n, cxt v, _Ref # cxt v)
+   _          -> ([assembleBinding cxt e], n+1, Local n, review _Ref . Local $ n)
+assemble n cxt (Core.Let bs e) =
+  Let bs' . assemble (n + genericLength bs) cxt' $ fromScope e
+ where
+ cxt' (F v) = cxt v
+ cxt' (B b) = Local $ n + b
+ bs' = assembleBinding cxt' . fromScope <$> bs
+assemble _ _   (Core.Data _    _ _ _ ) = error "assemble: Data"
+assemble _ _   (Core.Dict _ _) = error "assemble: Dict"
+assemble _ _   (Core.CaseLit _ _ _ _) = error "assemble: CaseLit"
+
+-- TODO: handle calling conventions
+assembleBranches :: Eq v
+                 => Word32
+                 -> Ref
+                 -> (v -> Ref)
+                 -> Map Word8 (Match Core v)
+                 -> Maybe (Scope () Core v)
+                 -> Continuation
+assembleBranches n ev cxt bs d = Cont bs' d'
+ where
+ bs' = bs <&> \(Match ccvs _ e) ->
+   let cxt' = unvar bc cxt
+       bc 0 = ev
+       bc b = Local . (n-1+) . fromIntegral $ b
+    in assemble (n+genericLength ccvs) cxt' (fromScope e)
+ d' = assemble n (unvar (const ev) cxt) . fromScope <$> d
+
+anf :: Eq v
+    => Word32 -> (v -> Ref)
+    -> LensLike (State (Word32, [PreClosure])) s t (Core v) Ref
+    -> s -> (t, (Word32, [PreClosure]))
+anf n cxt ll s = runState (ll assemblePiece s) (n, [])
+ where
+ assemblePiece (Core.Var v) = return $ cxt v
+ assemblePiece co = state $ \(k,l) ->
+   let bnd = assembleBinding cxt co
+    in (Local k,(k+1,bnd:l))
+
+assembleApp :: Eq v => Word32 -> (v -> Ref) -> [Core v] -> Core v -> Code
+assembleApp n cxt xs (Core.App _ f x) = assembleApp n cxt (x:xs) f
+assembleApp n cxt xs f = (if null bs then id else Let bs) $ App (Ref f') xs'
+ where
+ ((f', xs'), (_, bs)) = anf n cxt (beside id traverse) (f, xs)
+assembleHardCore :: HardCore -> Code
+assembleHardCore _ = undefined
 
 ------------------------------------------------------------------------------
 -- Evaluation
@@ -114,17 +220,17 @@ data MachineState m = MachineState
   }
 
 allocClosure :: (Functor m, PrimMonad m)
-             => GEnv m -> LEnv m -> ([Var], LambdaForm) -> m (Address m)
+             => GEnv m -> LEnv m -> PreClosure -> m (Address m)
 allocClosure gl lo cc = Address <$> (newMutVar $ buildClosure gl lo cc)
 
 allocRecursive :: (Applicative m, PrimMonad m)
-               => GEnv m -> LEnv m -> [([Var], LambdaForm)] -> m (LEnv m)
+               => GEnv m -> LEnv m -> [PreClosure] -> m (LEnv m)
 allocRecursive gl lo ccs = do
   addrs <- for ccs $ \_ -> Address <$> newMutVar undefined
   let lo' = extend lo $ Addr <$> addrs
   (lo<$) . sequence_ $ zipWith (flip poke . buildClosure gl lo') ccs addrs
 
-buildClosure :: GEnv m -> LEnv m -> ([Var], LambdaForm) -> Closure m
+buildClosure :: GEnv m -> LEnv m -> PreClosure -> Closure m
 buildClosure gl lo (captures, code) = Closure code (resolve gl lo <$> captures)
 
 poke :: PrimMonad m => Address m -> Closure m -> m ()
@@ -137,7 +243,7 @@ extend :: Integral k => Map k a -> [a] -> Map k a
 extend lo vs = fromList ([w ..] `zip` vs) `union` lo
  where w = fresh lo
 
-resolve :: GEnv m -> LEnv m -> Var -> Value m
+resolve :: GEnv m -> LEnv m -> Ref -> Value m
 resolve gl _  (Global gw) = case lookup gw gl of
   Nothing -> error $ "PANIC: bad global name reference: " ++ show gw
   Just ad -> Addr ad
@@ -174,7 +280,7 @@ select (Cont bs df) t =
 eval :: (Applicative m, PrimMonad m)
      => MachineState m -> Code -> LEnv m -> m (Either (Closure m) Word64)
 eval ms (App f xs0) lo = case f of
-  Var v -> case resolve gl lo v of
+  Ref r -> case resolve gl lo r of
     Addr a -> enter (pushArgs xs ms) a
     Prim w | null xs0  -> returnLit ms w
            | otherwise -> error "PANIC: primitive applied to arguments"
@@ -200,9 +306,9 @@ enter ms addr = peek addr >>= \case
     | ar <= length argz -> eval (ms & args .~ rest) (co^.body) lenv
     | otherwise -> case pop ms of
       Just (Update ad st, ms') -> do
-        let nf = co^.freeArity + fromIntegral (length argz)
-            nb = co^.boundArity - fromIntegral (length argz)
-        poke ad $ Closure (LambdaForm nf nb False $ co^.body) (Addr addr : argz)
+        let nf = co^.freeArity + genericLength argz
+            nb = co^.boundArity - genericLength argz
+        poke ad $ Closure (noUpdate nf nb $ co^.body) (Addr addr : argz)
         enter (ms' & args %~ (++st)) addr
       Just (Branch _ _,_) -> error "under-applied function in branch"
       Nothing -> return $ Left cl
@@ -218,11 +324,11 @@ returnCon ms t vs = case pop ms of
   Just (Branch k lo, ms') -> eval ms' (select k t) (extend lo vs)
   Just (Update ad st, ms')
     | null $ ms'^.args -> do
-      poke ad (Closure (standardConstructor (fromIntegral $ length vs) t) vs)
+      poke ad (Closure (standardConstructor (genericLength vs) t) vs)
       returnCon (ms' & args .~ st) t vs
     | otherwise -> error "PANIC: update with non-empty arg stack"
   Nothing -> return . Left $
-    Closure (standardConstructor (fromIntegral $ length vs) t) vs
+    Closure (standardConstructor (genericLength vs) t) vs
 
 returnLit :: PrimMonad m => MachineState m -> Word64 -> m (Either (Closure m) Word64)
 returnLit _ _ = error "returnLit: unimplemented"
