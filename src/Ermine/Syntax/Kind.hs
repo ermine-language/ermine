1b5436e7d5a17ba9c1d2a66ab4b050042211e806
Edward Kmett <ekmett@gmail.com>
2014-06-19 17:44:02 -0400

    remove dubious Hinted instance

--- src/Ermine/Syntax/Kind.hs
+++ src/Ermine/Syntax/Kind.hs
@@ -268,9 +268,6 @@ instance HasKindVars s t a b => HasKindVars (IntMap s) (IntMap t) a b where
 instance HasKindVars s t a b => HasKindVars (Map k s) (Map k t) a b where
   kindVars = traverse.kindVars
 
-instance HasKindVars s t a b => HasKindVars (Hinted s) (Hinted t) a b where
-  kindVars = traverse.kindVars
-
 ------------------------------------------------------------------------------
 -- Schema
 ------------------------------------------------------------------------------
