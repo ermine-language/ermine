620f5d431906712d6dff665d2a40013c96a78696
Stephen Compall <scompall@spcapitaliq.com>
2014-10-02 17:37:00 -0400

    A generalized source loader type with an associated cache entry.

---
+++ src/Ermine/Loader/Core.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TemplateHaskell #-}
+--------------------------------------------------------------------
+-- |
+-- Copyright :  (c) McGraw Hill Financial 2014
+-- License   :  BSD3
+-- Maintainer:  Stephen Compall <scompall@nocandysw.com>
+-- Stability :  experimental
+-- Portability: non-portable
+--
+--------------------------------------------------------------------
+
+module Ermine.Loader.Core
+  (
+  ) where
+
+import Control.Lens
+import Control.Monad
+
+-- | A pure loader or reloader.
+data Loader e n m a = Loader
+  { _load :: n -> m (e, a)
+  , _reload :: n -> e -> m (e, a)
+  }
+
+makeLenses ''Loader
+
+instance Functor m => Functor (Loader e n m) where
+  fmap = trans . fmap . fmap
+
+-- | Lift a transformation on the results of a loader.
+trans :: (m (e, a) -> m' (e, a'))
+      -> Loader e n m a
+      -> Loader e n m' a'
+trans f l = l {_load = f . _load l,
+               _reload = fmap f . _reload l}
+
+-- | A loader without reloadability.
+alwaysFresh :: Functor m => (n -> m a) -> Loader () n m a
+alwaysFresh f = Loader (fmap ((),) . f) (const . fmap ((),) . f)
+
+-- | Contramap the name parameter.
+contramapName :: (n' -> n) -> Loader e n m a -> Loader e n' m a
+contramapName f l = l {_load = _load l . f,
+                       _reload = _reload l . f}
+
+-- | Compose two loaders.
+compose :: Monad m => Loader e2 a m b -> Loader e n m a
+                   -> Loader (e, e2) n m b
+compose (Loader {_load = l2, _reload = r2})
+        (Loader {_load = l1, _reload = r1}) =
+  Loader (l1 >=> \(e, a) -> (_1 %~ (e,)) `liftM` l2 a)
+         (\n (e, e2) -> do
+             (e', a) <- r1 n e
+             (_1 %~ (e',)) `liftM` r2 a e2)
