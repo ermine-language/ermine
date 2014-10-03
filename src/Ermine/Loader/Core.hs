2907210b8a2d3eea3d68c38efa3a179e61aee95b
Stephen Compall <scompall@spcapitaliq.com>
2014-10-03 11:01:10 -0400

    Further lens things, xmap the e in Loader.

--- src/Ermine/Loader/Core.hs
+++ src/Ermine/Loader/Core.hs
@@ -35,6 +35,11 @@ trans :: (m (e, a) -> m' (e, a'))
 trans f l = l {_load = f . _load l,
                _reload = fmap f . _reload l}
 
+xmapCacheKey :: Functor m => AnIso' e e2 -> Loader e n m a -> Loader e2 n m a
+xmapCacheKey i l = withIso i $ \t f ->
+  l {_load = _load l & mapped.mapped._1 %~ t,
+     _reload = (\q -> (mapped._1 %~ t) . q . f) . _reload l}
+
 -- | A loader without reloadability.
 alwaysFresh :: Functor m => (n -> m a) -> Loader () n m a
 alwaysFresh f = Loader (fmap ((),) . f) (const . fmap ((),) . f)
@@ -49,7 +54,7 @@ compose :: Monad m => Loader e2 a m b -> Loader e n m a
                    -> Loader (e, e2) n m b
 compose (Loader {_load = l2, _reload = r2})
         (Loader {_load = l1, _reload = r1}) =
-  Loader (l1 >=> \(e, a) -> (_1 %~ (e,)) `liftM` l2 a)
+  Loader (l1 >=> \(e, a) -> l2 a & lifted._1 %~ (e,))
          (\n (e, e2) -> do
              (e', a) <- r1 n e
-             (_1 %~ (e',)) `liftM` r2 a e2)
+             r2 a e2 & lifted._1 %~ (e',))
