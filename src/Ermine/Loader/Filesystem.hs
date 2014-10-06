83645c1db9c06b2b764f771b34ecf88306c232aa
Stephen Compall <scompall@spcapitaliq.com>
2014-10-06 17:02:46 -0400

    Stub Filesystem loader.

---
+++ src/Ermine/Loader/Filesystem.hs
@@ -0,0 +1,13 @@
+--------------------------------------------------------------------
+-- |
+-- Copyright :  (c) McGraw Hill Financial 2014
+-- License   :  BSD3
+-- Maintainer:  Stephen Compall <scompall@nocandysw.com>
+-- Stability :  experimental
+-- Portability: non-portable
+--
+--------------------------------------------------------------------
+
+module Ermine.Loader.Filesystem
+  (
+  ) where
