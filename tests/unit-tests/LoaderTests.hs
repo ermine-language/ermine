355535397f911924f131f4d3df5514fb078a3bad
Stephen Compall <scompall@spcapitaliq.com>
2014-10-15 18:10:56 -0400

    Test that filesystemLoader recognizes bad module names.

--- tests/unit-tests/LoaderTests.hs
+++ tests/unit-tests/LoaderTests.hs
@@ -2,8 +2,10 @@
 module LoaderTests (main) where
 
 import Control.Lens
+import Control.Monad (forM_)
 import Control.Monad.Trans.Except
-import Data.Text (Text)
+import Data.Functor (void)
+import Data.Text (Text, pack)
 import qualified Data.Text.IO as TIO
 import Ermine.Loader.Core
 import Ermine.Loader.Filesystem
@@ -14,13 +16,25 @@ import Test.HUnit
 
 import ParserTests (stdLibDir)
 
-stdLibLoader :: Loader Freshness (ExceptT LoadRefusal IO) Text Text
-stdLibLoader = filesystemLoader stdLibDir "e"
+stdLibLoader :: Loader Freshness (ExceptT LoadRefusal IO) String Text
+stdLibLoader = contramapName pack $ filesystemLoader stdLibDir "e"
 
 loadsAFile = do fraw <- TIO.readFile (stdLibDir </> "Byte.e")
                 lr <- (stdLibLoader ^. load) "Byte" & runExceptT
                 assertEqual "load result" (Right fraw) (fmap snd lr)
 
+recognizesBadNames =
+  [".Byte", "Byte.", "Control..Monad", "Control/Monad", "Control\NULMonad", ""]
+   `forM_` \modName -> do
+    let badMod :: Either LoadRefusal a -> Assertion
+        badMod = assertEqual ("Not a valid module name: " ++ modName)
+                             (Left NoFilenameMapping) . void
+    lr <- (stdLibLoader ^. load) modName & runExceptT
+    badMod lr
+    rr <- (stdLibLoader ^. reload) modName undefined & runExceptT
+    badMod rr
+
 main = TestGroup "LoaderTests"
   [ testCase "loads a file" loadsAFile
+  , testCase "recognizes bad names" recognizesBadNames
   ]
