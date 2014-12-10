5169c161901e96bd0f9afdf59e51ef0662600400
Stephen Compall <scompall@spcapitaliq.com>
2014-12-10 15:21:36 -0500

    ErmineParserT, to tie the type-level knot.

--- src/Ermine/Parser/State.hs
+++ src/Ermine/Parser/State.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
 --------------------------------------------------------------------
 -- |
@@ -15,16 +16,25 @@
 --------------------------------------------------------------------
 
 module Ermine.Parser.State
-  ( ParseState(ParseState)
+  ( -- * Stateful parsing
+    ParseState(ParseState)
   , HasParseState(..)
   , initialParserState
+    -- * Parser combinator integration
+  , ErmineParserT(..)
   ) where
 
+import Control.Applicative (Alternative, Applicative)
 import Control.Lens
+import Control.Monad.State
 import Data.Typeable
 import Ermine.Loader.MapCache
 import Ermine.Syntax.Module
 import Ermine.Syntax.ModuleName
+import Text.Parser.Char (CharParsing)
+import Text.Parser.Combinators (Parsing)
+import Text.Parser.LookAhead (LookAheadParsing)
+import Text.Parser.Token (TokenParsing)
 
 data ParseState e m = ParseState
   { _stateFixities :: [FixityDecl]
@@ -46,3 +56,20 @@ instance HasCacheLoader (ParseState e m) e m ModuleName Module where
 -- | A parser at the beginning.
 initialParserState :: CacheLoader e m ModuleName Module -> ParseState e m
 initialParserState cl = ParseState [] cl []
+
+-- | A monad transformer that adds a 'StateT' of a self-referential
+-- 'ParseState' to its argument.
+newtype ErmineParserT e m a =
+  ErmineParserT { runErmineParserT ::
+                     StateT (ParseState e (ErmineParserT e m)) m a }
+  deriving (Functor, Applicative, Monad, Alternative, MonadPlus,
+            Parsing, CharParsing, LookAheadParsing, TokenParsing)
+
+instance Monad m => MonadState (ParseState e (ErmineParserT e m))
+                               (ErmineParserT e m) where
+  get = ErmineParserT get
+  put = ErmineParserT . put
+  state = ErmineParserT . state
+
+instance MonadTrans (ErmineParserT e) where
+  lift = ErmineParserT . lift
