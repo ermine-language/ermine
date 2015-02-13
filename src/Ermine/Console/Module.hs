81296e20ce27c91680ea8fd1ec82bf8eff911068
Stephen Compall <scompall@spcapitaliq.com>
2015-02-13 18:14:00 -0500

    Delete MonadCatch and liftCatchEx.

--- src/Ermine/Console/Module.hs
+++ src/Ermine/Console/Module.hs
@@ -1,12 +1,7 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE TypeFamilies #-}
-
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# OPTIONS_GHC -Wall -fdefer-type-errors #-}
 --------------------------------------------------------------------
 -- |
 -- Copyright :  (c) McGraw Hill Financial 2014-2015
@@ -34,16 +29,17 @@ import Data.Traversable (forM)
 import Data.Foldable (foldl', forM_)
 import Data.Monoid (mempty, (<>))
 import Data.Text (Text, unpack)
-import Ermine.Loader.Core (Loader, fanoutIdLoaderM, load,
-                           thenM, thenM')
--- import Ermine.Loader.Filesystem (filesystemLoader, Freshness, LoadRefusal)
+import Ermine.Loader.Core (Loader, contramapName, fanoutIdLoaderM,
+                           load, loaded, thenM, thenM')
+import Ermine.Loader.Filesystem (filesystemLoader, explainLoadRefusal,
+                                 Freshness)
 -- import Ermine.Loader.MapCache (loadCached)
 import Ermine.Parser.Module
 -- import Ermine.Parser.State
 import Ermine.Pretty hiding (string, int, integer, (</>))
 import Ermine.Syntax.Module (Import, Module, ModuleHead, importModule,
                              moduleHeadImports, moduleHeadText)
-import Ermine.Syntax.ModuleName (ModuleName, moduleName)
+import Ermine.Syntax.ModuleName (ModuleName, moduleName, mkModuleName_)
 import Ermine.Syntax.Name
 import System.FilePath ((</>))
 import Text.Parser.Combinators (eof)
@@ -52,16 +48,10 @@ import Text.Trifecta.Parser
 import Text.Trifecta.Result
 
 -- TODO s11 remove
-import Control.Monad.State hiding (forM, forM_)
-import Control.Monad.Trans
+import Control.Monad.State hiding (forM, forM_, liftM)
 import Control.Monad.Trans.Except
-import Control.Monad.Trans.Maybe
-import Control.Monad.Trans.Reader
-import Ermine.Instances
-import Ermine.Loader.Core
-import Ermine.Loader.Filesystem
+import Ermine.Instances ()
 import Ermine.Loader.MapCache
-import Ermine.Syntax.ModuleName
 
 -- | From a loader that loads the raw 'Text' of a module, produce a
 -- 'Module' loader using the cache in state.
@@ -93,34 +83,7 @@ moduleLoader l = fanoutIdLoaderM lemh `thenM'` afterModuleHead
           impss `forM_` importSet
           gets (HM.! n)
 
--- TODO s11: remove
-liftCatchEx :: (Monad m, MonadCatch a m) =>
-               (a -> b)
-            -> ExceptT b m x
-            -> ExceptT b (Catch m) x
-liftCatchEx f = mapExceptT (flip catchError' (return . Left . f))
-
-class (MonadError e m, Monad (Catch m)) => MonadCatch e m where
-  type Catch m :: * -> *
-  catchError' :: m a -> (e -> Catch m a) -> Catch m a
-
-instance Monad m => MonadCatch e (ExceptT e m) where
-  type Catch (ExceptT e m) = m
-  catchError' ma f = runExceptT ma >>= either f return
-
-instance MonadCatch e m => MonadCatch e (StateT s m) where
-  type Catch (StateT s m) = StateT s (Catch m)
-  catchError' ma f = StateT $ \s ->
-    catchError' (runStateT ma s) (flip runStateT s . f)
-
-instance MonadCatch e m => MonadCatch e (MaybeT m) where
-  type Catch (MaybeT m) = MaybeT (Catch m)
-  catchError' ma f = mapMaybeT (flip catchError' (runMaybeT . f)) ma
-
-instance MonadCatch e m => MonadCatch e (ReaderT r m) where
-  type Catch (ReaderT r m) = ReaderT r (Catch m)
-  catchError' ma f = ReaderT $ \r ->
-    catchError' (runReaderT ma r) (flip runReaderT r . f)
+-- TODO s11: remove or pull out to tests
 
 testLoader :: String -> IO (Either Doc Module)
 testLoader =
